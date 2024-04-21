24adc64df31522cc41fbefb56a342838fbb7447d
Crowdbotics <team@crowdbotics.com>
2024-04-21 10:26:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "smartbagz-platform-47653",
+  url: "https://smartbagz-platform-47653.botics.co",
+  api: "https://smartbagz-platform-47653.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
